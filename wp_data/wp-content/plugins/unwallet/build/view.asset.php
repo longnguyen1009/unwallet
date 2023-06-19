6eefa6ba299cf53275d6425619c0aed3f12ca77d
longnguyen1009 <l.ongbn95@gmail.com>
2023-06-19 07:23:05 +0700

    update unwallet plugin

--- wp_data/wp-content/plugins/unwallet/build/view.asset.php
+++ wp_data/wp-content/plugins/unwallet/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-element'), 'version' => '6899ecca1daad0d22f63');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-element'), 'version' => '73da27205ac4f4bf4ab9');
--- wp_data/wp-content/plugins/unwallet/src/view.js
+++ wp_data/wp-content/plugins/unwallet/src/view.js
@@ -1,7 +1,28 @@
 import domReady from '@wordpress/dom-ready';
 import { render } from "@wordpress/element";
+import { UnWallet } from "unwallet-client-sdk";
 
-const App = () => <div>REACT!</div>
+const nonce = "123456789";
+
+const App = function () {
+    async function loginClick() {
+        const unWallet = await UnWallet.init({
+            clientID: "133980164233664",
+            env: "dev", // ※ testnet 版の場合のみ指定
+        });
+
+        await unWallet.authorize({
+            redirectURL: "http://localhost:8000/sample-page/",
+            nonce: nonce,
+        });
+    }
+
+    return (
+      <button onClick={loginClick}>
+        Click me!
+      </button>
+    );
+}
 
 domReady( function () {
     //do something after DOM loads.
